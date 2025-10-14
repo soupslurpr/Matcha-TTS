26a86334f1e6d8153618cd5481e05f2adc33a48c
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-10-14 15:24:54 -0700

    update model export code to use dynamo and split architecture

--- matcha/onnx/export.py
+++ matcha/onnx/export.py
@@ -10,6 +10,7 @@ from torch.export import Dim
 
 from matcha.cli import VOCODER_URLS, load_matcha, load_vocoder
 from matcha.models.matcha_tts import MatchaTTS
+from matcha.utils.model import fix_len_compatibility, sequence_mask, generate_path, denormalize
 
 DEFAULT_OPSET = 21
 
@@ -22,63 +23,128 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
 
-class MatchaWithVocoder(LightningModule):
-    def __init__(self, matcha, vocoder):
+class MatchaEncoder(LightningModule):
+    def __init__(self, matcha: MatchaTTS):
         super().__init__()
-        self.matcha = matcha
-        self.vocoder = vocoder
+        self.n_spks = matcha.n_spks
+        if self.n_spks > 1:
+            self.spk_emb = matcha.spk_emb
+        self.encoder = matcha.encoder
 
-    def forward(self, x, x_lengths, temperature, length_scale, spks=None):
-        mel, mel_lengths = self.matcha(x, x_lengths, temperature, length_scale, spks)
-        wavs = self.vocoder(mel).clamp(-1, 1)
-        lengths = mel_lengths * 256
-        return wavs.squeeze(1), lengths
+    def forward(self, x: Tensor, x_lengths: Tensor, length_scale: Tensor, spks: Tensor = None):
+        if self.n_spks > 1:
+            # Get speaker embedding
+            spks = self.spk_emb(spks.long())
+
+        # Get encoder_outputs `mu_x` and log-scaled token durations `logw`
+        mu_x, logw, x_mask = self.encoder(x, x_lengths, spks)
+
+        w = torch.exp(logw) * x_mask
+        w_ceil = torch.ceil(w) * length_scale
+        y_lengths = torch.clamp_min(torch.sum(w_ceil, [1, 2]), 1).long()
+        y_max_length = y_lengths.max()
+        y_max_length_ = fix_len_compatibility(y_max_length)
+
+        # Using obtained durations `w` construct alignment map `attn`
+        y_mask = sequence_mask(y_lengths, y_max_length_).unsqueeze(1).to(x_mask.dtype)
+        attn_mask = x_mask.unsqueeze(-1) * y_mask.unsqueeze(2)
+        attn = generate_path(w_ceil.squeeze(1), attn_mask.squeeze(1)).unsqueeze(1)
+
+        # Align encoded text and get mu_y
+        mu_y = torch.matmul(attn.squeeze(1).transpose(1, 2), mu_x.transpose(1, 2))
+        mu_y = mu_y.transpose(1, 2)
+
+        if spks is None:
+            return y_lengths, mu_y, y_mask
+        else:
+            return y_lengths, mu_y, y_mask, spks
 
 
-def get_exportable_module(matcha: MatchaTTS, vocoder: LightningModule, n_timesteps: int):
+def get_exportable_encoder(encoder: MatchaEncoder):
     """
     Return an appropriate `LighteningModule` and output-node names
-    based on whether the vocoder is embedded in  the final graph
     """
 
-    def onnx_forward_func(x: Tensor, x_lengths: Tensor, temperature: Tensor, length_scale: Tensor, spks: Tensor = None):
-        """
-        Custom forward function for accepting
-        scaler parameters as tensors
-        """
-        output = matcha.synthesise(x, x_lengths, n_timesteps, temperature[0], spks, length_scale[0])
-        return output["mel"], output["mel_lengths"]
-
-    # Monkey-patch Matcha's forward function
-    matcha.forward = onnx_forward_func
-
-    if vocoder is None:
-        model, output_names = matcha, ["mel", "mel_lengths"]
-    else:
-        model = MatchaWithVocoder(matcha, vocoder)
-        output_names = ["wav", "wav_lengths"]
+    model, output_names = encoder, ["y_lengths", "mu_y", "y_mask", "spks"]
     return model, output_names
 
 
-def get_inputs(is_multi_speaker):
+def get_encoder_inputs(is_multi_speaker: bool):
     """
-    Create dummy inputs for tracing
+    Create dummy encoder inputs for tracing
     """
-    dummy_input_length = 50
+    dummy_input_length = 5000
     x = torch.randint(low=0, high=20, size=(1, dummy_input_length), dtype=torch.long)
     x_lengths = torch.LongTensor([dummy_input_length])
+    length_scale = torch.FloatTensor([1.0])
 
-    temperature = 0.667
-    length_scale = 1.0
-    temperature = torch.Tensor([temperature])
-    length_scale = torch.Tensor([length_scale])
-
-    model_inputs = [x, x_lengths, temperature, length_scale]
+    model_inputs = [x, x_lengths, length_scale]
     input_names = [
         "x",
         "x_lengths",
-        "temperature",
         "length_scale",
+    ]
+
+    if is_multi_speaker:
+        spks = torch.LongTensor([1])
+        model_inputs.append(spks)
+        input_names.append("spks")
+
+    return tuple(model_inputs), input_names
+
+
+class MatchaDecoder(LightningModule):
+    def __init__(self, matcha: MatchaTTS):
+        super().__init__()
+        # self.n_spks = matcha.n_spks
+        # if self.n_spks > 1:
+        #     self.spk_emb = matcha.spk_emb
+        self.decoder = matcha.decoder
+        self.mel_mean = matcha.mel_mean
+        self.mel_std = matcha.mel_std
+
+        self.vocoder, _ = load_vocoder("generator_v3", "hifi-gan/upstream-trained-models/LJ_V3/generator_v3", "cpu")
+
+    def forward(self, mu_y: Tensor, y_mask: Tensor, n_timesteps: Tensor, temperature: Tensor, spks: Tensor = None):
+        # TODO: if n_timesteps can't be dynamic, move the forward parameter to __init__ to make it static
+        n_timesteps = torch.tensor(5)
+
+        # Generate sample tracing the probability flow
+        decoder_outputs = self.decoder(mu_y, y_mask, n_timesteps, temperature, spks)
+
+        mel = denormalize(decoder_outputs, self.mel_mean, self.mel_std)
+
+        wavs = self.vocoder(mel).clamp(-1, 1)
+        return wavs.squeeze(1)
+
+        # return denormalize(decoder_outputs, self.mel_mean, self.mel_std)
+
+
+def get_exportable_decoder(decoder: MatchaDecoder):
+    """
+    Return an appropriate `LighteningModule` and output-node names
+    """
+
+    model, output_names = decoder, ["mel"]
+    return model, output_names
+
+
+def get_decoder_inputs(out_size: int, is_multi_speaker: bool):
+    """
+    Create dummy decoder inputs for tracing
+    """
+    dummy_input_length = out_size
+    mu_y = torch.rand(size=[1, 80, dummy_input_length])
+    y_mask = torch.rand(size=[1, 1, dummy_input_length])
+    n_timesteps = torch.tensor(5)
+    temperature = torch.tensor(0.667)
+
+    model_inputs = [mu_y, y_mask, n_timesteps, temperature]
+    input_names = [
+        "mu_y",
+        "y_mask",
+        "n_timesteps",
+        "temperature",
     ]
 
     if is_multi_speaker:
@@ -97,22 +163,9 @@ def main():
         type=str,
         help="Path to the model checkpoint",
     )
-    parser.add_argument("output", type=str, help="Path to output `.onnx` file")
+    parser.add_argument("output", type=str, help="Path to output folder")
     parser.add_argument(
         "--n-timesteps", type=int, default=5, help="Number of steps to use for reverse diffusion in decoder (default 5)"
-    )
-    parser.add_argument(
-        "--vocoder-name",
-        type=str,
-        choices=list(VOCODER_URLS.keys()),
-        default=None,
-        help="Name of the vocoder to embed in the ONNX graph",
-    )
-    parser.add_argument(
-        "--vocoder-checkpoint-path",
-        type=str,
-        default=None,
-        help="Vocoder checkpoint to embed  in the ONNX graph for an `e2e` like experience",
     )
     parser.add_argument("--opset", type=int, default=DEFAULT_OPSET, help="ONNX opset version to use (default 21")
 
@@ -124,72 +177,62 @@ def main():
     checkpoint_path = Path(args.checkpoint_path)
     matcha = load_matcha(checkpoint_path.stem, checkpoint_path, "cpu")
 
-    if args.vocoder_name or args.vocoder_checkpoint_path:
-        assert (
-            args.vocoder_name and args.vocoder_checkpoint_path
-        ), "Both vocoder_name and vocoder-checkpoint are required when embedding the vocoder in the ONNX graph."
-        vocoder, _ = load_vocoder(args.vocoder_name, args.vocoder_checkpoint_path, "cpu")
-    else:
-        vocoder = None
-
     is_multi_speaker = matcha.n_spks > 1
 
-    dummy_input, input_names = get_inputs(is_multi_speaker)
-    model, output_names = get_exportable_module(matcha, vocoder, args.n_timesteps)
-    model = model.eval()
+    encoder_dummy_input, encoder_input_names = get_encoder_inputs(is_multi_speaker)
+    encoder, encoder_output_names = get_exportable_encoder(MatchaEncoder(matcha))
+    encoder = encoder.eval()
 
-    # Set dynamic shape for inputs/outputs
-    dynamic_axes = {
-        "x": {0: "batch_size", 1: "time"},
-        "x_lengths": {0: "batch_size"},
-    }
-
-    if vocoder is None:
-        dynamic_axes.update(
-            {
-                "mel": {0: "batch_size", 2: "time"},
-                "mel_lengths": {0: "batch_size"},
-            }
-        )
-    else:
-        print("Embedding the vocoder in the ONNX graph")
-        dynamic_axes.update(
-            {
-                "wav": {0: "batch_size", 1: "time"},
-                "wav_lengths": {0: "batch_size"},
-            }
-        )
-
-    if is_multi_speaker:
-        dynamic_axes["spks"] = {0: "batch_size"}
+    decoder_dummy_input, decoder_input_names = get_decoder_inputs(matcha.out_size, is_multi_speaker)
+    # TODO: if n_timesteps needs to be static, make it a parameter to MatchaDecoder
+    decoder, decoder_output_names = get_exportable_decoder(MatchaDecoder(matcha))
+    decoder = decoder.eval()
 
     # Create the output directory (if not exists)
-    Path(args.output).parent.mkdir(parents=True, exist_ok=True)
+    Path(args.output).mkdir(parents=True, exist_ok=True)
 
     # static batch size for exported model
     batch_size = Dim.STATIC
     max_text_length = Dim("max_text_length")
-    temperature = Dim.STATIC
     length_scale = Dim.STATIC
-    dynamic_shapes = {
+    encoder_dynamic_shapes = {
         "x": {0: batch_size, 1: max_text_length},
         "x_lengths": {0: batch_size},
-        "temperature": {0: temperature},
-        "length_scale": {0: length_scale}
+        "length_scale": {0: length_scale},
+    }
+
+    y_max_length_dim = Dim.STATIC
+    decoder_dynamic_shapes = {
+        "mu_y": {0: batch_size, 2: y_max_length_dim},
+        "y_mask": {0: batch_size, 2: y_max_length_dim},
+        "n_timesteps": {},
+        "temperature": {},
     }
 
     with torch.inference_mode():
-        model.to_onnx(
-            args.output,
-            dummy_input,
-            input_names=input_names,
-            output_names=output_names,
-            dynamic_axes=dynamic_axes,
+        encoder.to_onnx(
+            f"{args.output}/encoder.onnx",
+            encoder_dummy_input,
+            input_names=encoder_input_names,
+            output_names=encoder_output_names,
+            dynamic_shapes=encoder_dynamic_shapes,
             opset_version=args.opset,
             export_params=True,
-            dynamo=False,
+            dynamo=True,
+            external_data=False,
         )
-    print(f"[🍵] ONNX model exported to  {args.output}")
+        decoder.to_onnx(
+            f"{args.output}/decoder.onnx",
+            decoder_dummy_input,
+            input_names=decoder_input_names,
+            output_names=decoder_output_names,
+            dynamic_shapes=decoder_dynamic_shapes,
+            opset_version=args.opset,
+            export_params=True,
+            dynamo=True,
+            external_data=False,
+        )
+    print(f"[🍵] ONNX model exported to {args.output}")
 
 
 if __name__ == "__main__":
